b3ccbec246580686293c8e0f4a6c3505ad1e2297
MrRex <souvikhazra151@gmail.com>
2024-12-02 01:32:31 +0530

    done

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,6 +1,6 @@
 export const DB_NAME = "follow_up";
 export const COOKIE_OPTIONS = {
   httpOnly: true,
-  secure: false,
-  // sameSite: "none" as "none",
+  secure: true,
+  sameSite: "none" as const,
 };
